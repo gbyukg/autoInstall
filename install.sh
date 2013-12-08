1b5d0898a4d6246e4072fedffa3bc048f255c054
Lock <zeliangz@cn.ibm.com>
2013-12-08 14:46:27 +0800

    add url install

--- install.sh
+++ install.sh
@@ -21,6 +21,9 @@ mas_branch=""
 fet_branch=""
 install_meth=""
 install_name=""
+download_url="http://sugjnk01.rtp.raleigh.ibm.com"
+man_url=""
+down_file=""
 star="********************"
 
 err_hand()
@@ -84,9 +87,15 @@ pre_git()
 
 pre_url()
 {
-  echo ""
-  echo "选择的是URL安装方式"
-  echo ""
+  cus_echo "选择的是URL安装方式"
+  cus_echo "开始下载文件:${down_file}"
+  [ -d sugarcrm ] && rm -rf sugarcrm
+  wget -O sugarcrm.zip ${download_url}
+  cus_echo "开始解压"
+  unzip -d sugarcrm sugarcrm.zip &> /dev/null
+  [ -d ${WEB_DIR}/${install_name} ] && cus_echo "删除原有文件${WEB_DIR}/${install_name}" && rm -rf ${WEB_DIR}/${install_name}
+  cp -r sugarcrm/SugarUlt-Full-6.7.0 ${WEB_DIR}/${install_name}
+  rm -rf sugarcrm.zip
 }
 
 install()
@@ -111,7 +120,7 @@ install()
   cus_echo "第一.二步"
   curl -o install.html -b cookies.cook -d "checkInstallSystem=true&to_pdf=1&sugar_body_only=1" http://localhost/${install_name}/install.php >&/dev/null 2>&1
   sleep 3
-  
+
   cus_echo "第二步"
   curl -o install.html -b cookies.cook -d "setup_license_accept=on&current_step=2&goto=Next" http://localhost/${install_name}/install.php >&/dev/null 2>&1
   sleep 3
@@ -119,27 +128,23 @@ install()
   cus_echo "第三步"
   curl -o install.html -b cookies.cook -d "setup_license_key=${KEY}&install_type=custom&current_step=3&goto=Next" http://localhost/${install_name}/install.php >&/dev/null 2>&1
   sleep 3
-  
+
   cus_echo "第四步"
   curl -o install.html -b cookies.cook -d "setup_db_type=ibm_db2&current_step=4&goto=Next" http://localhost/${install_name}/install.php >&/dev/null 2>&1
   sleep 3
-  
-  
+
   cus_echo "check database"
   curl -o install.html -b cookies.cook -d "checkDBSettings=true&to_pdf=1&sugar_body_only=1&setup_db_database_name=${db_name}&setup_db_port_num=${DB_PORT}&setup_db_host_name=${DB_HOST}&setup_db_admin_user_name=${DB_USER}&setup_db_admin_password=${DB_PWD}&demoData=no" http://localhost/${install_name}/install.php >&/dev/null 2>&1
   sleep 3
-  
-  
+
   cus_echo "第五步"
   curl -o install.html -b cookies.cook -d "checkDBSettings=true&setup_db_drop_tables=false&goto=Next&setup_db_database_name=${db_name}&setup_db_host_name=${DB_HOST}&setup_db_prot_num=&setup_db_create_sugarsales_user=&setup_db_admin_user_name=${DB_USER}&setup_db_admin_password_entry=${DB_PWD}&setup_db_admin_password=${DB_PWD}&demoData=no&current_step=5" http://localhost/${install_name}/install.php >&/dev/null 2>&1
   sleep 3
-  
-  
+
   cus_echo "第六步"
   curl -o install.html -b cookies.cook -d "current_step=6&goto=Next&setup_site_url=${SITE_URL}&setup_system_name=SugarCRM&setup_site_admin_user_name=${SITE_USER}&setup_site_admin_password=${SITE_PWD}&setup_site_admin_password_retype=${SITE_PWD}" http://localhost/${install_name}/install.php >&/dev/null 2>&1
   sleep 3
-  
-  
+
   cus_echo "第七步"
   curl -o install.html -b cookies.cook -d "current_step=7&goto=Next&setup_site_sugarbeet_anonymous_stats=yes&setup_site_session_path=&setup_site_log_dir=&setup_site_guid=" http://localhost/${install_name}/install.php >&/dev/null 2>&1
 
@@ -167,18 +172,14 @@ init_db()
   expect ${SCRIPT_DIR}/initdb.exp ${DB_USER} ${DB_PWD} ${db_name} ${INITDB_PATH}
 }
 
-data_loader()
+data_config()
 {
-  cus_echo "data loader"
-  [[ "XGIT" == "X${install_meth}" ]] &&
-    {
-      cd ${GIT_DIR}/ibm/dataloaders
     cat <<CONFIG > config.php
 <?php
 
 \$config = array(
 
-        // DB settings 
+        // DB settings
         'db' => array(
                 'type' => 'db2', // mysql or db2
                 'host' => '127.0.0.1',
@@ -204,22 +205,6 @@ data_loader()
 
 );
 CONFIG
-
-      php populate_SmallDataset.php && git checkout ${GIT_DIR}/ibm/dataloaders/config.php
-
-      cd ${WEB_DIR}/${install_name}/custom/cli
-      php -f cli.php task=RebuildClientHierarchy && php -f cli.php task=UpdateUsersTopTierNode
-
-      cd ${WEB_DIR}/${install_name}/batch_sugar/RTC_19211
-      php -f rtc_19211_main.php RTC_19211>&/dev/null 2>&1
-
-      cp -r ${GIT_DIR}/sugarcrm/tests ${WEB_DIR}/${install_name}
-      chmod 755 ${WEB_DIR}/${install_name}/tests/phpunit.php ${WEB_DIR}/${install_name}/tests/phpunit2.php
-
-    } || 
-    {
-      echo 'url'
-    }
 }
 
 add_ignore()
@@ -244,16 +229,45 @@ add_project()
 	</projects>
 	<buildSpec>
 		<buildCommand>
-			<name>org.eclipse.wst.common.project.facet.core.builder</name>
+			<name>org.eclipse.wst.validation.validationbuilder</name>
+			<arguments>
+			</arguments>
+		</buildCommand>
+		<buildCommand>
+			<name>org.eclipse.dltk.core.scriptbuilder</name>
 			<arguments>
 			</arguments>
 		</buildCommand>
 	</buildSpec>
 	<natures>
-		<nature>org.eclipse.wst.common.project.facet.core.nature</nature>
+		<nature>org.eclipse.php.core.PHPNature</nature>
 	</natures>
 </projectDescription>
 PROJECT
+}
+
+data_loader()
+{
+  cus_echo "data loader"
+  [[ "XGIT" == "X${install_meth}" ]] &&
+    {
+      cd ${GIT_DIR}/ibm/dataloaders
+      cp -r ${GIT_DIR}/sugarcrm/tests ${WEB_DIR}/${install_name}
+      chmod 755 ${WEB_DIR}/${install_name}/tests/phpunit.php ${WEB_DIR}/${install_name}/tests/phpunit2.php
+    } ||
+    {
+      cd ${SCRIPT_DIR}/sugarcrm/ibm/dataloaders
+    }
+      data_config
+
+      php populate_SmallDataset.php && git checkout ${GIT_DIR}/ibm/dataloaders/config.php
+
+      cd ${WEB_DIR}/${install_name}/custom/cli
+      php -f cli.php task=RebuildClientHierarchy && php -f cli.php task=UpdateUsersTopTierNode
+
+      cd ${WEB_DIR}/${install_name}/batch_sugar/RTC_19211
+      php -f rtc_19211_main.php RTC_19211>&/dev/null 2>&1
+
 }
 
 after_install()
@@ -279,11 +293,27 @@ after_install()
 
   # 删除安装文件信息
   rm -rf *.html cookies.cook
-  
+
   cus_echo "安装完成"
-  google-chrome http://localhost/${install_name}/index.php
+  type google-chrome > /dev/null 2>&1 && google-chrome http://localhost/${install_name}/index.php || 
+  {
+      type chromium-browser > /dev/null 2>&1 && chromium-browser google-chrome http://localhost/${install_name}/index.php || 
+      {
+        firefox http://localhost/${install_name}/index.php
+      }
+  }
 }
 
+install_name=sugarcrm
+
+  type google-chrome > /dev/null 2>&1 && google-chrome http://localhost/${install_name}/index.php || 
+  {
+      type chromium-browser > /dev/null 2>&1 && chromium-browser google-chrome http://localhost/${install_name}/index.php || 
+      {
+        firefox http://localhost/${install_name}/index.php
+      }
+  }
+  exit 0
 # 入口文件
 for i in $@; do
   [[ "X${i}" == 'X--debug' ]] && set -x && shift && break
@@ -293,6 +323,12 @@ while [ "$1" != '' ]; do
   case $1 in
     -h | --help )
       echo "help"
+      echo '-g/--git git check主分支 自己分支'
+      echo '-u/--url url 包名'
+      echo '--db_name dbName git:默认gitsugar; url默认urlsugar'
+      echo '--mas-remote upstream'
+      echo '--fet-remote origin'
+      echo '--debug'
       exit 0
       ;;
     --mas-remote )
@@ -321,8 +357,11 @@ while [ "$1" != '' ]; do
       install_meth="URL"
       shift
       get_db_name
-      # gen_install_name
-      
+      man_url=${1:?"必须指定下载路径"} && download_url="${download_url}/${1}" && shift
+      down_file=${1:?"必须指定下载文件名"} && download_url="${download_url}/${1}"  && shift
+      down_file=${down_file%\.*}
+      gen_install_name url ${down_file} 0
+      echo ${install_name}
       ;;
     * )
       shift
@@ -330,11 +369,9 @@ while [ "$1" != '' ]; do
 esac
 done
 
-[[ -z ${install_meth} ]] && bash ${SCRIPT_DIR}/install.sh --help
-
+[[ -z ${install_meth} ]] && bash ${SCRIPT_DIR}/install.sh --help && exit 1
 # 准备安装
 [[ "X${install_meth}" == "XGIT" ]] && time pre_git || time pre_url
-
 # 开始安装
 time install
 
